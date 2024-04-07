264abc6fa8594ade756b76b55afb87bd3470cd2a
Saul Consuegra Moran <saul.consuegra.moran.26@gmail.com>
2024-04-07 17:05:40 -0500

    commit 2

--- src/Tarker.Booking.Application/Database/Customer/Queries/GetCustomers/GetAllCustomersQuery.cs
+++ src/Tarker.Booking.Application/Database/Customer/Queries/GetCustomers/GetAllCustomersQuery.cs
@@ -18,7 +18,7 @@ namespace Tarker.Booking.Application.Database.Customer.Queries.GetCustomers
             _dataBaseService = dataBaseService; 
             _mapper = mapper;
         }
-        ///VCINGOIEHRG
+        ///VCINGOIEHRGf
         public async Task<List<GetAllCustomersModel>> Execute(int customerId)
         {
             var entity = await _dataBaseService.Customers.ToListAsync();
